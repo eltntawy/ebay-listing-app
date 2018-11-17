67d1d1eb89a2e9966161445980ce8b0c7046fe05
Mohamed Hassan <mohamed.eltantawy@backbase.com>
2018-11-17 23:26:27 +0100

    add rest template buffer factory

--- src/main/java/com/app/listing/ebay/utils/RestTemplateUtil.java
+++ src/main/java/com/app/listing/ebay/utils/RestTemplateUtil.java
@@ -1,22 +1,29 @@
 package com.app.listing.ebay.utils;
 
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class RestTemplateUtil {
-    private static RestTemplate rt = new RestTemplate();
+    private static RestTemplate restTemplate;
 
     static {
-        List<ClientHttpRequestInterceptor> ris = new ArrayList<ClientHttpRequestInterceptor>();
-        ris.add(new LoggingRequestInterceptor());
-        rt.setInterceptors(ris);
+        ClientHttpRequestFactory factory = new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory());
+
+        restTemplate = new RestTemplate(factory);
+
+        List<ClientHttpRequestInterceptor> requestInterceptors = new ArrayList<ClientHttpRequestInterceptor>();
+        requestInterceptors.add(new LoggingRequestInterceptor());
+        restTemplate.setInterceptors(requestInterceptors);
     }
 
     public static RestTemplate getRestTemplate() {
-        return rt;
+        return restTemplate;
     }
 
 }
