5b6bcaa9cd86d6b70b86bcdd6ea4c47ea617ee5a
Mohamed Hassan <mohamed.eltantawy@backbase.com>
2018-11-18 00:02:55 +0100

    add rest template buffer factory

--- src/main/java/com/app/listing/ebay/services/InventoryService.java
+++ src/main/java/com/app/listing/ebay/services/InventoryService.java
@@ -3,16 +3,12 @@ package com.app.listing.ebay.services;
 import com.mycompany.myapp.ebay.api.ApiClient;
 import com.mycompany.myapp.ebay.api.auth.OAuth;
 import com.mycompany.myapp.ebay.api.inventory.InventoryItemApi;
-import com.mycompany.myapp.ebay.api.inventory.model.BulkInventoryItem;
-import com.mycompany.myapp.ebay.api.inventory.model.BulkInventoryItemResponse;
-import com.mycompany.myapp.ebay.api.inventory.model.InventoryItemWithSkuLocaleGroupid;
-import com.mycompany.myapp.ebay.api.inventory.model.InventoryItems;
-import io.swagger.annotations.Authorization;
+import com.mycompany.myapp.ebay.api.inventory.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import sun.rmi.runtime.Log;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -29,8 +25,12 @@ public class InventoryService {
     @Autowired
     AuthService authService;
 
-    public List<InventoryItemWithSkuLocaleGroupid> getAllItems() {
+    @PostConstruct
+    public void init () {
         apiClient.setAccessToken(authService.getTokenResponseDto().getAccessToken());
+    }
+
+    public List<InventoryItemWithSkuLocaleGroupid> getAllItems() {
 
         // Configure OAuth2 access token for authorization: Authorization Code
         OAuth authorizationCode = (OAuth) apiClient.getAuthentication("Authorization Code");
@@ -47,5 +47,73 @@ public class InventoryService {
 
         return null;
 
+    }
+
+
+    /**
+     * item should be look like the below
+     * {
+     *    "requests":[
+     *       {
+     *          "sku":"MQCL2LL/A",
+     *          "product":{
+     *             "title":"Boston Terriers Collector Plate &quot;All Ears� by Dan Hatala - The Danbury Mint",
+     *             "aspects":{
+     *                "Country/Region of Manufacture":[
+     *                   "United States"
+     *                ]
+     *             },
+     *             "description":"All Ears by Dan Hatala. A limited edition from the collection entitled 'Boston Terriers'. Presented by The Danbury Mint.",
+     *             "imageUrls":[
+     *                "https://i.ebayimg.com/00/s/OTk5WDc0OQ==/z/54IAAOSw3thbW20V/$_12.JPG?set_id=880000500F"
+     *             ]
+     *          },
+     *          "condition":"USED_EXCELLENT",
+     *          "conditionDescription":"Mint condition. Kept in styrofoam case. Never displayed.",
+     *          "availability":{
+     *             "shipToLocationAvailability":{
+     *                "quantity":2
+     *             }
+     *          }
+     *       }
+     *    ]
+     * }
+     * @return
+     */
+    public List<InventoryItemResponse> createItemOrItems() {
+        // Configure OAuth2 access token for authorization: Authorization Code
+        OAuth authorizationCode = (OAuth) apiClient.getAuthentication("Authorization Code");
+        authorizationCode.setAccessToken(authService.getTokenResponseDto().getAccessToken());
+
+        InventoryItemApi apiInstance = new InventoryItemApi(apiClient);
+
+        try {
+            BulkInventoryItem bulkInventoryItem = new BulkInventoryItem();
+
+            InventoryItemWithSkuLocale item = new InventoryItemWithSkuLocale();
+            item.setLocale("en_US");
+            item.setSku("MQCL2LL/A");
+            item.setConditionDescription("USED_EXCELLENT");
+
+
+            Product product = new Product();
+            product.setTitle("IPhone X test test test");
+            product.setBrand("IPhone");
+//            product.setAspects();
+            item.setProduct(product);
+
+            Availability availability = new Availability();
+            availability.setShipToLocationAvailability(new ShipToLocationAvailability().quantity(2));
+            item.setAvailability(availability);
+
+            bulkInventoryItem.addRequestsItem(item);
+
+            BulkInventoryItemResponse bulkInventoryItemResponse = apiInstance.bulkCreateOrReplaceInventoryItem(bulkInventoryItem);
+            return bulkInventoryItemResponse.getResponses();
+        } catch (Exception e) {
+            logger.log(Level.SEVERE,"Exception when calling InventoryItemApi#bulkCreateOrReplaceInventoryItem",e);
+        }
+
+        return null;
     }
 }
